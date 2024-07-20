bcf99b28c1b2eeb263213cb7629173020d4109c0
Shubham Tyagi <shubham2tyagi7@gmail.com>
2024-07-20 20:51:16 +0530

    remove unused libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,6 @@ android {
         targetSdk = 34
         versionCode = 7
         versionName = "1.4"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -33,12 +31,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-  //  composeOptions {
-   //     kotlinCompilerExtensionVersion = "1.5.1"
-  //  }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -47,32 +39,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    //implementation(libs.androidx.activity.compose)
-  //  implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-  //  implementation(libs.androidx.ui.graphics)
-   // implementation(libs.androidx.ui.tooling.preview)
-   // implementation(libs.androidx.material3)
     implementation (libs.material)
     implementation(libs.appcompat)
     implementation(libs.preference)
     implementation(libs.libphonenumber)
     implementation(libs.phonefield)
-
-    implementation(libs.androidx.constraintlayout)
-
-    testImplementation(libs.junit)
-   // androidTestImplementation(libs.androidx.junit)
-   // androidTestImplementation(libs.androidx.espresso.core)
-   // androidTestImplementation(platform(libs.androidx.compose.bom))
-  //  androidTestImplementation(libs.androidx.ui.test.junit4)
-
-  //  debugImplementation(libs.androidx.ui.tooling)
-   // debugImplementation(libs.androidx.ui.test.manifest)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
 }
